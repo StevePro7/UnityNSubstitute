10054471b1f6b3822d60dc4f7ca0160b8e9c9feb
StevePro7 <steveprozune@hotmail.com>
2018-01-29 15:00:43 +0000

    Update project

---
+++ UnitTestingPartOne/Assets/Editor/PizzaOrdererTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using NSubstitute;
+
+public class PizzaOrderTests
+{
+	private const string PIZZA_NAME = "Unity pizza with extra cheese";
+
+	[Test]
+	public void OrderedSuccessfully()
+	{
+		// Arrange.
+		IPaymentValidator paymentValidatorMock = GetValidatorMock();
+		IServerConnector connectorMock = GetConnectorMock();
+		var pizzaOrderer = new PizzaOrderer(paymentValidatorMock, connectorMock);
+
+		// Act.
+		pizzaOrderer.MakeOrder(PIZZA_NAME);
+
+		// Assert.
+		connectorMock.Received(1).SendRequestForPizza(Arg.Any<string>());
+	}
+
+	private IPaymentValidator GetValidatorMock()
+	{
+		var mock = Substitute.For<IPaymentValidator>();
+		mock.IsPaymentAccepted().Returns(true);
+		return mock;
+	}
+
+	private IServerConnector GetConnectorMock()
+	{
+		return Substitute.For<IServerConnector>();
+	}
+
+}
---
+++ UnitTestingPartOne/Assets/Scripts/PizzaOrderer.cs
@@ -0,0 +1,19 @@
+﻿public class PizzaOrderer
+{
+	public PizzaOrderer(IPaymentValidator paymentValidator, IServerConnector serverConnector)
+	{
+		this.paymentValidator = paymentValidator;
+		this.serverConnector = serverConnector;
+	}
+
+	public void MakeOrder(string pizzaName)
+	{
+		if (paymentValidator.IsPaymentAccepted())
+		{
+			serverConnector.SendRequestForPizza(pizzaName);
+		}
+	}
+
+	private IPaymentValidator paymentValidator;
+	private IServerConnector serverConnector;
+}
