4a85267a722bbc395f659523d51a22c02f412d63
StevePro7 <steveprozune@hotmail.com>
2018-01-29 15:21:38 +0000

    Update space ship

--- UnitTestingPartTwo/Assets/Scripts/SpaceshipMotor.cs
+++ UnitTestingPartTwo/Assets/Scripts/SpaceshipMotor.cs
@@ -12,43 +12,67 @@ public class SpaceshipMotor : MonoBehaviour
 		if (Input.GetButton("Horizontal"))
 		{
 			Debug.Log("Horizontal");
-			float deltaX = Time.fixedDeltaTime * Input.GetAxis("Horizontal") * (health >= 50 ? normalSpeed : woundedSpeed);
-			transform.Translate(deltaX, 0, 0);
+			MoveHorizontally(Input.GetAxis("Horizontal"));
 		}
 		if (Input.GetButton("Vertical"))
 		{
 			Debug.Log("Vertical");
-			float deltaY = Time.fixedDeltaTime * Input.GetAxis("Vertical") * (health >= 50 ? normalSpeed : woundedSpeed);
-			transform.Translate(0, deltaY, 0);
+			MoveVertically(Input.GetAxis("Vertical"));
 		}
 		if (Input.GetButton("Fire1"))
 		{
 			// Left ctrl.
 			Debug.Log("Fire1");
-			if (bulletsLeft > 0 && (lastFireTime + shootRate) < Time.time)
+			if (bulletsLeft > 0 && CanFire())
 			{
 				lastFireTime = Time.time;
 				bulletsLeft--;
-
-				var bullet = Instantiate(Bullet, GunMuzzle.transform.position, Quaternion.identity) as GameObject;
-				bullet.transform.parent = this.transform.parent;
+				InstantiateBullet();
 			}
 		}
 		if (Input.GetButton("Fire2"))
 		{
 			// Left alt.
 			Debug.Log("Fire2");
-			bulletsLeft = bulletCapacity;
+			Reload();
 		}
 	}
 
+	private void MoveHorizontally(float value)
+	{
+		float deltaX = Time.fixedDeltaTime * value * (health >= 50 ? normalSpeed : woundedSpeed);
+		TransformPosition(deltaX, 0f);
+	}
+	private void MoveVertically(float value)
+	{
+		float deltaY = Time.fixedDeltaTime * value * (health >= 50 ? normalSpeed : woundedSpeed);
+		TransformPosition(0f, deltaY);
+	}
+	private void TransformPosition(float deltaX, float deltaY)
+	{
+		transform.Translate(deltaX, deltaY, 0);
+	}
+	private bool CanFire()
+	{
+		return (lastFireTime + shootRate) < Time.time;
+	}
+	private void InstantiateBullet()
+	{
+		var bullet = Instantiate(Bullet, GunMuzzle.transform.position, Quaternion.identity) as GameObject;
+		bullet.transform.parent = this.transform.parent;
+	}
+	private void Reload()
+	{
+		bulletsLeft = bulletCapacity;
+	}
+
 	private const int bulletCapacity = 5;
-	private const float normalSpeed = 15f;
-	private const float shootRate = 0.5f;
-	private const float woundedSpeed = 3;
 
 	private int bulletsLeft = 5;
 	private float health = 100f;
 
 	private float lastFireTime = float.NegativeInfinity;
+	private const float normalSpeed = 15f;
+	private const float shootRate = 0.5f;
+	private const float woundedSpeed = 3;
 }
