19aab090218e9e90f70cfc1abdb95a9cbedf6d4b
StevePro7 <steveprozune@hotmail.com>
2018-01-29 15:31:17 +0000

    Update code

--- UnitTestingPartTwo/Assets/Scripts/Interfaces/IGunController.cs
+++ UnitTestingPartTwo/Assets/Scripts/Interfaces/IGunController.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
-namespace Assets.Scripts.Interfaces
+public interface IGunController
 {
-	class IGunController
-	{
-	}
 }
